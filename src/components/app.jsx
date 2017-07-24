0ebcfd2d57d65d585642c8ddd1bb325f09b9d91d
Kangseon Cho <kangseon.cho@gmail.com>
2017-07-23 23:34:43 -0700

    add function to allow gallery state to be loaded and initialize skeleton format of the site

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Image from './image.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      testState: 'testing'
+      gallery: []
     };
     //list of functions to pass down
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/gallery')
+    .then(res => this.setState({ gallery: res.data }));
+  }
+
   //make functions to pass down
   render() {
     return (
       <div>
-        <div>test test 123</div>
-        <div>{this.state.testState}</div>
+        {/* search bar and logo */}
+        <div id='navBar'>
+          <span id="logo">Gallery.js</span>
+          <span id="searchBar"> <input type="text"></input> </span>
+        </div>
+        {/*images*/}
+        <div id="gallery">
+          <Image gallery={this.state.gallery}/>
+        </div>
       </div>
     )
   }
