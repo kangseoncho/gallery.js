ecac3426880d991bd617628d8f46985c9923bd18
Kangseon Cho <kangseon.cho@gmail.com>
2017-07-24 23:41:01 -0700

    change func in lifecycle method to match the new route, add load more image button

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,23 +9,48 @@ class App extends Component {
       gallery: []
     };
     //list of functions to pass down
+    this.loadMoreArt = this.loadMoreArt.bind(this);
+    this.artistName = this.artistName.bind(this);
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3000/gallery')
+    axios.get('http://localhost:3000/initialGallery')
     .then(res => this.setState({ gallery: res.data }));
   }
 
+  artistName (string) {
+    let firstName = string.replace(/, /, ',').split(',');
+    let lastName = firstName.splice(0, 1);
+    return `${firstName.join('')} ${lastName}`
+  }
+
   //make functions to pass down
+  loadMoreArt () {
+    axios.get('http://localhost:3000/gallery')
+    .then(moreArts => {
+      let tempGallery = this.state.gallery;
+      tempGallery = tempGallery.concat(moreArts.data);
+
+      this.setState({ gallery: tempGallery })
+    });
+  }
+
   render() {
     return (
       <div>
+
         {/* search bar and logo */}
         <div id='navBar'>
-          <span id="logo">Gallery.js</span>
+          <div id="logo">Gallery.js</div>
         </div>
+
         {/*images*/}
-          <Image gallery={this.state.gallery} />
+        <Image gallery={this.state.gallery} artistName={this.artistName}/>
+
+        {/* button to load more artworks */}
+         <div id='loadImages'>
+          <button onClick={()=>{this.loadMoreArt()}}>Load More Artworks</button>
+         </div>
       </div>
     )
   }
