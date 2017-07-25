9970c52a3f51b46c7a5035f8f06e4dac45e5eaf1
Kangseon Cho <kangseon.cho@gmail.com>
2017-07-24 23:41:30 -0700

    change how artist's name was presented

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,14 +3,25 @@ import React from 'react';
 const Image = (props) => {
   let thumbnails = props.gallery.map((element, index) => {
     return (
-    <div className="painting" key={index}>
-      <div className="image">
-        <img src={element.thumbnailUrl} alt="paintings"></img>
+      <div className="painting" key={index}>
+
+        <div className="image">
+          <img src={element.thumbnailUrl} alt="paintings"></img>
+        </div>
+
+        <div className="title">
+          {element.title}
+        </div>
+
+        <div className="artist">
+           {props.artistName(element.artist)}
+        </div>
+
+        <div className="year">
+          {element.year}
+        </div>
+
       </div>
-      <div className="artist">{element.artist}</div>
-      <div className="title">{element.title}</div>
-      <div className="year">{element.year}</div>
-    </div>
     )
   })
 
